61926e86c5b0193dd406a0dc3e178d93232ac4e5
William Phokompe <phokompe.william@outlook.com>
2021-05-11 14:07:04 +0200

    add meals to navigation

--- navigation/ChowNavigator.js
+++ navigation/ChowNavigator.js
@@ -1,7 +1,8 @@
 import { createStackNavigator } from 'react-navigation-stack'
 import Categories from '../screens/Categories'
+import Meals from '../screens/Meals'
 
 createStackNavigator({
     Category: Categories,
-    
+    Meal: Meals
 });
