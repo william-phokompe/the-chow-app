7dc790be682d5acb5cfd2c48e790635ada3a3a83
William Phokompe <phokompe.william@outlook.com>
2021-05-11 13:12:27 +0200

    load fonts behind a splash screen

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import * as Font from "expo-font";
 import { AppLoading } from "expo-app-loading";
 
 const fetchFonts = _ => {
-  Font.loadAsync({
+  return Font.loadAsync({
     'bebas-neue-reg': require('./assets/fonts/Bebas-Neue-Regular.ttf'),
     'bebas-neue-bold': require('./assets/fonts/Bebas-Neue-Bold.otf')
   });
 }
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={_ => setFontLoaded(true)}/>
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
