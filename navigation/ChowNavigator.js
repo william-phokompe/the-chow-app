edc507e4d702406428823198031b5dcf52288d85
William Phokompe <phokompe.william@outlook.com>
2021-05-11 14:15:23 +0200

    export chow nav

--- navigation/ChowNavigator.js
+++ navigation/ChowNavigator.js
@@ -1,10 +1,14 @@
-import { createStackNavigator } from 'react-navigation-stack'
+import { createStackNavigator, createAppContainer } from 'react-navigation-stack'
 import Categories from '../screens/Categories'
 import MealRecipe from '../screens/MealRecipe';
 import Meals from '../screens/Meals'
 
-createStackNavigator({
+const ChowNavigator = createStackNavigator({
     Category: Categories,
-    Meal: Meals,
+    Meals: {
+        screen: Meals
+    },
     Recipe: MealRecipe
 });
+
+export default createAppContainer(ChowNavigator);
