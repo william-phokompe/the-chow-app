05174df009faf9fc09554024b4b2a0047d75d2f1
William Phokompe <phokompe.william@outlook.com>
2021-05-11 14:12:18 +0200

    add MealReciple screen to nav

--- navigation/ChowNavigator.js
+++ navigation/ChowNavigator.js
@@ -1,9 +1,10 @@
 import { createStackNavigator } from 'react-navigation-stack'
 import Categories from '../screens/Categories'
+import MealRecipe from '../screens/MealRecipe';
 import Meals from '../screens/Meals'
 
 createStackNavigator({
     Category: Categories,
     Meal: Meals,
-    
+    Recipe: MealRecipe
 });
---
+++ screens/MealRecipe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native'
+
+const MealRecipe = () => {
+    return (
+        <View style={styles.screen}>
+            <Text></Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default MealRecipe;
