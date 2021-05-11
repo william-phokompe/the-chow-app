b6282f5950d0993f7141c1e8aaefeeeabee605e0
William Phokompe <phokompe.william@outlook.com>
2021-05-11 13:02:17 +0200

    create screens folder and categories screen

---
+++ screens/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+const Categories = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Categories;
