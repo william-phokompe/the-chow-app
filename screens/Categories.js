d8660e03d5888dbd079f16f4f84189700da60d27
William Phokompe <phokompe.william@outlook.com>
2021-05-12 19:48:47 +0200

    out-source the grid and style the category screen

--- screens/Categories.js
+++ screens/Categories.js
@@ -1,29 +1,22 @@
 import React from "react";
-import { enableScreens } from 'react-native-screens';
-import {
-  StyleSheet,
-  FlatList,
-  View,
-  Text,
-  TouchableOpacity,
-} from "react-native";
+import { enableScreens } from "react-native-screens";
+import { StyleSheet, FlatList } from "react-native";
+
 import { CATEGORIES } from "../data/mock-data";
+import GridTiles from "../components/GridTiles";
 
 enableScreens();
 
 const Categories = (props) => {
   const renderGridItem = (itemData) => {
     return (
-      <TouchableOpacity
-        style={styles.grid}
-        onPress={(_) => {
+      <GridTiles
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onTap={(_) => {
           props.navigation.navigate("Chow", { categoryId: itemData.item.id });
         }}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+      />
     );
   };
 
@@ -46,12 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-
-  grid: {
-    flex: 1,
-    margin: 15,
-    height: 150,
   },
 });
 
