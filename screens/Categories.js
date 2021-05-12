40413d822eb8f68b7a116898f65a275f7d01a491
William Phokompe <phokompe.william@outlook.com>
2021-05-12 15:53:11 +0200

    move grid to the component and make the categories touchable

--- screens/Categories.js
+++ screens/Categories.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import { StyleSheet, FlatList } from 'react-native'
+import React from "react";
+import {
+  StyleSheet,
+  FlatList,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { CATEGORIES } from "../data/mock-data";
 
 const Categories = (props) => {
-    console.log(props)
+  const renderGridItem = (itemData) => {
     return (
-        <FlatList numColumns={2} />
+      <TouchableOpacity
+        style={styles.grid}
+        onPress={(_) => {
+          props.navigation.navigate("Meals");
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
     );
-}
+  };
+
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  grid: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
+});
 
 export default Categories;
