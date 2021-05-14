51adbcaf9817724795c726b7c0550fc540b23d5d
William Phokompe <phokompe.william@outlook.com>
2021-05-14 10:32:39 +0200

    ✨ Add content to Filters screen

--- components/screens/Filters.js
+++ components/screens/Filters.js
@@ -1,39 +1,91 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Switch, Platform } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../HeaderButton";
+import Colors from "../../constants/Colors";
 
-const Filters = () => {
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: Colors.secondary }}
+        thumbColor={Platform.OS === "android" ? Colors.secondary : ""}
+        value={props.switchState}
+        onValueChange={props.onSwitch}
+      />
+    </View>
+  );
+};
+
+const Filters = (props) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text>Filters Screen</Text>
+      <Text style={styles.title}>Available Filters</Text>
+      <FilterSwitch
+        label="Gluten-Free"
+        switchState={isGlutenFree}
+        onSwitch={(newValue) => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label="Lactose-Free"
+        switchState={isLactoseFree}
+        onSwitch={(newValue) => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        switchState={isVegan}
+        onSwitch={(newValue) => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        switchState={isVegetarian}
+        onSwitch={(newValue) => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
 
 Filters.navigationOptions = (navigationData) => {
-    return {
-      headerTitle: "Filtered meals",
-      headerLeft: (
-        <HeaderButtons HeaderButtonComponent={HeaderButton}>
-          <Item
-            title="Menu"
-            iconName="ios-menu"
-            onPress={(_) => {
-              navigationData.navigation.toggleDrawer();
-            }}
-          />
-        </HeaderButtons>
-      ),
-    };
+  return {
+    headerTitle: "Filtered meals",
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={(_) => {
+            navigationData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
+    marginHorizontal: 20,
     alignContent: "center",
+  },
+  title: {
+    fontFamily: "bebas-neue-reg",
+    fontSize: 30,
+    margin: 20,
+    textAlign: "center",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 15
   },
 });
 
